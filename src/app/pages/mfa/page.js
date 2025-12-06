b15918aa631d17579728085a0d63973f15c15603
Therushmorare <tshego.tt.morare@gmail.com>
2025-12-06 22:27:42 +0200

    mfa bug fix

--- src/app/pages/mfa/page.js
+++ src/app/pages/mfa/page.js
@@ -1,16 +1,7 @@
 "use client";
 
-import { useRouter } from "next/router";
-import MFA from "../../components/auth/Mfa";
+import MFA from "../../components/auth/MFA";
 
 export default function MFAPage() {
-  const router = useRouter();
-
-  const handleVerify = (data) => {
-    console.log("MFA Verified:", data);
-    // Redirect to dashboard after MFA
-    router.push("/dashboard");
-  };
-
-  return <MFA onVerify={handleVerify} />;
+  return <MFA />;
 }
